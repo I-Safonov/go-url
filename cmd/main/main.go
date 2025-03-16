0bac9db37acc7ce14d5ea4e8ceccbec3fa8f8bae
I-Safonov <safivan05@yandex.ru>
2025-03-16 10:05:55 +0300

    add auth.go with funcs: GenerateToken, GetUserID; start to develop api/handlers.go.

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,15 +1,35 @@
 package main
 
 import (
-	"net/http"
+	"url-shortener/internal/api"
+	"url-shortener/internal/auth"
+	"url-shortener/internal/storage"
 
+	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello World")
-	})
+
+	shards := storage.InitShards()
+	rdb := storage.NewRedisClient()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(api.MetricsMiddleware())
+
+	authGroup := e.Group("/api")
+	authGroup.Use(echojwt.WithConfig(echojwt.Config{
+		SigningKey:  []byte(auth.SecretKey),
+		TokenLookup: "header:Authorization",
+	}))
+
+	authGroup.POST("/shorten", api.ShortenURL(shards, rdb))
+	e.GET("/:code", api.RedirectURL(shards, rdb))
+
+	e.GET("/metrics", api.PrometheusHandler())
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
